97f0188ec11bc7e48916ddbaef6017c90eb3245e
yusufONU1 <yusuf.ibrahim@cgi.com>
2023-04-21 08:23:44 +0200

    Search functionality implemented

--- smartShopper/src/app/View/search/search-routing.module.ts
+++ smartShopper/src/app/View/search/search-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SearchPage } from './search.page';
+import { SearchPage } from './controller/search.page';
 
 const routes: Routes = [
   {
